29da107b1f5ae181ece94b65583c977483c5216b
Rw7-0 <zaxt1642@gmail.com>
2024-11-09 02:36:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,15 +13,3 @@ if (isset($_POST)){
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
