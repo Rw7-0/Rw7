7003b1ffdd70c0bc48cec4efa78744c14800e281
Rw7-0 <zaxt1642@gmail.com>
2024-11-09 03:00:02 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+$username ="1";
+$password ="1";
+if (isset($_POST)){
+    $getusername = $_POST['username'];
+    $getpassword = $_POST['password'];
+    
+    if ($username === $getusername && $password === $getpassword) {
+          echo "تم تسجيل الدخول";
+          header('location: login.php');
+    } else {
+        echo "  ";
+    }
+}
+?> 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
